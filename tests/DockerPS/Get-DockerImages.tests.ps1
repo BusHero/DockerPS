59da0bd86b57bf502d60e06814f1d149b3c18f9c
Cervac Petru <petru.cervac@gmail.com>
2022-10-15 20:50:22 +0300

    feat: create Get-DockerImages function (#2)
    * feat: create Get-DockerImages function
    
    * feat: Improve Get-DockerImages
    
    Add support for specifying repository
    
    * feat: add Format-DockerArguments function
    
    Add a function to format the arguments that will be sent to docker
    
    * feat: add support for the --no-trunc flag
    
    * feat: add support for the --filter flags
    
    * feat: add support for the --filter flags
    
    * refr: refactor Format-DockerArguments
    
    move the formating of filters to separate functions
    
    * feat: add Get-DockerImages function
    
    Add the Get-DockerImages function and replace all the tests to test it
    instead of the Format-DockerArguments function.
    
    * feat: make Get-DockerImages return an object
    
    Add the ConvertFrom-Json to convert the json returned by the docker
    command to an object
    
    * chore: rename main.ps1 to Get-DockerImages.ps1
    
    Rename files to express the function that files implement:
    main.ps1 ==> Get-DockerImages.ps1
    main.tests.ps1 ==> Get-DockerImages.tests.ps1
    
    * chore: add new line at the end
    
    * chore: remove trailing whitespace

---
+++ tests/DockerPS/Get-DockerImages.tests.ps1
@@ -0,0 +1,120 @@
+BeforeAll {
+	. (Get-ScriptPath -Path $PSCommandPath)
+}
+
+Describe 'Get-DockerImages' {
+	BeforeAll {
+		Mock docker {  }
+	}
+	
+	It 'No parameters' {
+		Get-DockerImages
+		Should -Invoke -CommandName 'docker' -Exactly -Times 1 -ParameterFilter {
+			"$args" -eq "images --format '{{json .}}'"
+		}
+	}
+
+	It 'Specify image name' {
+		$imageName = 'foo'
+		Get-DockerImages -Image $imageName
+		Should -Invoke -CommandName 'docker' -Times 1 -ParameterFilter {
+			"images --format '{{json .}}' $imageName" 
+		}
+	}
+
+	It '--no-trunk' {
+		Get-DockerImages -NoTrunk
+		Should -Invoke -CommandName 'docker' -Times 1 -ParameterFilter {
+			"$args" -eq "images --format '{{json .}}' --no-trunc"
+		}
+	}
+
+	Describe '--filter' {
+		It 'one filter' {
+			$filter = 'foo=bar'
+			Get-DockerImages -Filter $filter 
+			Should -Invoke -CommandName 'docker' -Times 1 -ParameterFilter {
+				"$args" -eq "images --format '{{json .}}' --filter '${filter}'"
+			}
+		}
+
+		It 'two filter' {
+			$filter1 = 'foo=bar'
+			$filter2 = 'bar=baz'
+			Get-DockerImages -Filter $filter1, $filter2
+			Should -Invoke -CommandName 'docker' -Times 1 -ParameterFilter {
+				"$args" -eq "images --format '{{json .}}' --filter '${filter1}' --filter '${filter2}'"
+			}
+		}
+
+		Describe 'Ignore empty filters' {
+			It 'One filter' {
+				$filter = ''
+				Get-DockerImages -Filter $filter
+				Should -Invoke -CommandName 'docker' -Times 1 -ParameterFilter {
+					"$args" -eq "images --format '{{json .}}'"
+				}
+			}
+			
+			It 'Two filters' {
+				$filter1 = 'foo=bar'
+				$filter2 = ''
+				Get-DockerImages -Filter $filter1, $filter2
+				Should -Invoke -CommandName 'docker' -Times 1 -ParameterFilter {
+					"$args" -eq "images --format '{{json .}}' --filter '${filter1}'"
+				}
+			}
+		}
+
+		Describe 'Filter as dictionary' {
+			It 'One filter' {
+				$filter = @{
+					foo = 'Bar'
+				}
+				Get-DockerImages -Filter $filter
+				Should -Invoke -CommandName 'docker' -Times 1 -ParameterFilter {
+					"$args" -eq "images --format '{{json .}}' --filter 'foo=$($filter.foo)'"
+				}
+			}
+
+			It 'Hashtable' {
+				$filter = @{
+					foo = 'bar'
+					bar = 'baz'
+				}
+				Get-DockerImages -Filter $filter
+				Should -Invoke -CommandName 'docker' -Times 1 -ParameterFilter {
+					switch ("${args}") {
+						"images --format '{{json .}}' --filter 'foo=$($filter.foo)' --filter 'bar=$($filter.bar)'" { return $true }
+						"images --format '{{json .}}' --filter 'bar=$($filter.bar)' --filter 'foo=$($filter.foo)'" { return $true }
+						Default { return $false }
+					}
+				}
+			}
+
+			It 'Two filter ordered' {
+				$filter = [ordered]@{
+					foo = 'Bar'
+					bar = 'Baz'
+				}
+				Get-DockerImages -Filter $filter
+				Should -Invoke -CommandName 'docker' -Times 1 -ParameterFilter {
+					"$args' -eq 'images --format '{{json .}}' --filter 'foo=$($filter.foo)' --filter 'bar=$($filter.bar)'"
+				}
+			}
+		}
+	}
+
+	Describe 'Get-DockerImages returns an object' {
+		BeforeAll {
+			Mock docker { '{"foo": "bar", "bar": "baz"}' }
+			$result = Get-DockerImages
+		}
+		It 'Contains foo property' {
+			$result.foo | Should -Be 'bar'
+		}
+		It 'Contains bar property' {
+			$result.bar | Should -Be 'baz'
+		}
+	}
+}
