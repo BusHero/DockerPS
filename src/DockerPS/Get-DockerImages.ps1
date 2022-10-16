b426dea2e57e3fd0b00e93371e7eb95ffffa9297
bus1hero <petru.cervac@gmail.com>
2022-10-16 19:04:51 +0300

    chore: Update help uri link

--- src/DockerPS/Get-DockerImages.ps1
+++ src/DockerPS/Get-DockerImages.ps1
@@ -72,7 +72,7 @@ function Format-DockerArguments {
 }
 
 function Get-DockerImages {
-	[CmdletBinding(HelpUri = 'https://github.com/BusHero/DockerPS')]
+	[CmdletBinding(HelpUri = 'https://github.com/BusHero/DockerPS/blob/main/docs/Get-DockerImages.md')]
 	param (
 		[string]
 		$Image,
