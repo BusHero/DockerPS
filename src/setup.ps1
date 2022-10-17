2236afdc911f22e8db67ab9d1bff009056618f3e
Cervac Petru <petru.cervac@gmail.com>
2022-10-17 20:58:22 +0300

    feat: impoved the testing of the package (#9)
    Test the package that will be uploaded to nuget directly

--- src/setup.ps1
+++ src/setup.ps1
@@ -4,11 +4,10 @@ $manifestPath = "${PSScriptRoot}\$($constants.ProjectName)\$($constants.ProjectN
 New-ModuleManifest `
 	-Guid $constants.ProjectGUID `
 	-Path $manifestPath `
+	-Description 'Some description here and there' `
 	-Author $constants.Author `
 	-ProjectUri $constants.Repository `
 	-LicenseUri $constants.LicenseUri `
 	-RootModule "$($constants.ProjectName).psm1" `
 	-Tags 'docker' `
 	-FunctionsToExport 'Get-DockerImages'
-
-return $manifestPath
--- tests/setup.tests.ps1
+++ tests/setup.tests.ps1
@@ -1,22 +1,55 @@
 BeforeAll {
 	$constants = & "${PSScriptRoot}\..\constants.ps1"
-	$ModulePath = & (Get-ScriptPath -Path $PSCommandPath)
-	Import-Module $ModulePath -DisableNameChecking
+	Get-ScriptPath -Path $PSCommandPath
+	
+	$PSRepository = "LocalPSRepo_$(New-Guid)"
+	$PackagesDirectory = "${TestDrive}\packages"
+	
+	New-Item `
+		-Path $PackagesDirectory `
+		-ItemType Directory `
+		-Force
+		
+	Register-PSRepository `
+		-Name $PSRepository `
+		-SourceLocation $PackagesDirectory `
+		-ScriptSourceLocation $PackagesDirectory `
+		-InstallationPolicy Trusted
+		
+	Publish-Module `
+		-Path "${PSScriptRoot}\..\src\$($constants.ProjectName)" `
+		-Repository $PSRepository
+	
+	Install-Module `
+		-Name $constants.ProjectName `
+		-Repository $PSRepository `
+		-Scope CurrentUser `
+		-SkipPublisherCheck `
+		-Force
+	Import-Module `
+		-Name $constants.ProjectName `
+		-Scope Local
 	$ImportedModule = Get-Module $constants.ProjectName
 }
-
+	
 Describe 'Check project' {
 	It 'Project GUID' {
 		$ImportedModule.Guid | Should -Be $constants.ProjectGUID
 	}
-
+		
 	Describe 'Author' {
 		It 'Should be specified' {
 			$ImportedModule.Author | Should -Not -BeNullOrEmpty
 		}
-
+			
 		It 'As in constants' {
 			$ImportedModule.Author | Should -Be $constants.Author
+		}
+	}
+
+	Describe 'Description' {
+		It 'Should be specified' {
+			$ImportedModule.Description | Should -Not -BeNullOrEmpty
 		}
 	}
 
@@ -84,9 +117,16 @@ Describe 'Check project' {
 	}
 }
 
-
 AfterAll {
 	Remove-Module `
 		-Name $ImportedModule `
+		-ErrorAction Ignore
+
+	Uninstall-Module `
+		-Name DockerPS `
+		-ErrorAction Ignore
+
+	Unregister-PSRepository `
+		-Name $PSRepository `
 		-ErrorAction Ignore
 }
