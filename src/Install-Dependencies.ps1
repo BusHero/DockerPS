4d463e04df681cadf42556eb80e4d212c1cdce86
Cervac Petru <petru.cervac@gmail.com>
2022-10-18 20:13:24 +0300

    chore: add PSScriptAnalyzer to the dependencies (#12)

--- src/Install-Dependencies.ps1
+++ src/Install-Dependencies.ps1
@@ -1,4 +1,20 @@
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-Set-PSRepository -Name PSGallery -InstallationPolicy Trusted 
-Install-Module -Name Pester -Force
-Install-Module -Name PesterExtensions -Force
+Set-PSRepository `
+	-Name PSGallery `
+	-InstallationPolicy Trusted 
+
+Write-Host 'Install Pester'
+Install-Module `
+	-Name Pester `
+	-RequiredVersion '5.3.3' `
+	-Force
+Write-Host 'Install PesterExtensions'
+Install-Module `
+	-Name PesterExtensions `
+	-RequiredVersion '0.7.4' `
+	-Force
+Write-Host 'Install PSScriptAnalyzer'
+Install-Module `
+	-Name PSScriptAnalyzer `
+	-RequiredVersion '1.21.0' `
+	-Force
