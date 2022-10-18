d1ce7916b55c536b12d92493735e8b424de9a27b
bus1hero <petru.cervac@gmail.com>
2022-10-18 20:12:30 +0300

    chore: add PSScriptAnalyzer to the dependencies

--- tests/dependencies.Tests.ps1
+++ tests/dependencies.Tests.ps1
@@ -1,6 +1,7 @@
 Describe 'Check dependencies are installed' -ForEach @(
 	@{ ModuleName = 'Pester'; Version = '5.3.3' }
 	@{ ModuleName = 'PesterExtensions'; Version = '0.7.4' }
+	@{ ModuleName = 'PSScriptAnalyzer'; Version = '1.21.0' }
 ) {
 	It '<ModuleName> should have <Version>' {
 		$module = Get-InstalledModule ${ModuleName} 
