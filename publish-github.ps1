f102f52299b5a55388a0b424a656154f018e448a
bus1hero <petru.cervac@gmail.com>
2022-10-18 22:57:46 +0300

    feat: update the build workflow
    Make the build workflow to build the package

---
+++ publish-github.ps1
@@ -0,0 +1,19 @@
+$PSRepository = 'LocalPSRepo'
+$PackagesDirectory = "${PSScriptRoot}\packages"
+
+Write-Host 'Create .\packages'
+New-Item `
+	-Path $PackagesDirectory `
+	-ItemType Directory
+
+Write-Host 'Register local PSRepository'
+Register-PSRepository `
+	-Name $PSRepository `
+	-SourceLocation $PackagesDirectory `
+	-ScriptSourceLocation $PackagesDirectory `
+	-InstallationPolicy Trusted
+
+Write-Host 'Publish module'
+Publish-Module `
+	-Path '.\src\DockerPS' `
+	-Repository $PSRepository
