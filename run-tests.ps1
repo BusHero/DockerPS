c198fb67ee9af24ff880758bee9f0b4057bea42a
bus1hero <petru.cervac@gmail.com>
2022-10-18 22:18:22 +0300

    chore: add github actions output

--- run-tests.ps1
+++ run-tests.ps1
@@ -3,6 +3,7 @@ Import-Module Pester
 $configuration = [PesterConfiguration](New-PesterConfiguration)
 $configuration.Run.Path = 'tests'
 $configuration.Filter.ExcludeTag = 'docker'
+$configuration.Output.CIFormat = 'GithubActions'
 
 Invoke-Pester -Configuration $configuration
 
