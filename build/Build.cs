f32b6c69d0cbf98c9723fb785b7a13341777505d
bus1hero <petru.cervac@gmail.com>
2022-10-23 19:15:29 +0300

    feat: fix build project

--- build/Build.cs
+++ build/Build.cs
@@ -1,20 +1,19 @@
 using Nuke.Common;
 using Nuke.Common.IO;
-using Nuke.Common.Tooling;
+using Nuke.Common.Tools.PowerShell;
 using static Nuke.Common.Tools.PowerShell.PowerShellTasks;
+using Nuke.Common.Tooling;
 
 class Build : NukeBuild
 {
-	[PathExecutable]
-	readonly Tool Pwsh;
-
-	readonly AbsolutePath Foo = RootDirectory / "constants.ps1";
+	readonly AbsolutePath Foo = RootDirectory / "Foo.ps1";
 
 	public static int Main() => Execute<Build>(x => x.First);
 
-	Target First => _ => _
-		.Executes(() =>
-		{
-			PowerShell($"-NoProfile -File '{Foo}'");
-		});
+	private Target First => _ => _
+		.Executes(() => PowerShell(_ => _
+			.SetProcessToolPath("pwsh")
+			.SetNoProfile(true)
+			.SetNoLogo(true)
+			.SetFile(Foo)));
 }
