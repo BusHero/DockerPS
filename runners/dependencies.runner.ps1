7044722a7011222f7c448877446cae72bcefff8e
bus1hero <petru.cervac@gmail.com>
2022-10-23 21:14:32 +0300

    chore: refactor dependencies.runner.ps1
    Remove a variable that was used only in one place

--- runners/dependencies.runner.ps1
+++ runners/dependencies.runner.ps1
@@ -1,14 +1,14 @@
 Import-Module Pester
 
 $ProjectRoot = "${PSScriptRoot}\.."
-$path = "${ProjectRoot}\tests\dependencies.Tests.ps1"
 
 [PesterConfiguration] $configuration = New-PesterConfiguration
 
-$configuration.Run.Path = $path
+$configuration.Run.Path = "${ProjectRoot}\tests\dependencies.Tests.ps1"
 $configuration.Run.Exit = $true
 $configuration.Output.CIFormat = 'GithubActions'
 $configuration.TestResult.Enabled = $true
-$configuration.TestResult.OutputPath = "${ProjectRoot}\test-results\dependencies.Tests.$(Get-Date -Format 'yyyy_MM_dd_HH_mm_ss').xml"
+$configuration.TestResult.OutputPath = `
+	"${ProjectRoot}\test-results\dependencies.Tests.$(Get-Date -Format 'yyyy_MM_dd_HH_mm_ss').xml"
 
 Invoke-Pester -Configuration $configuration
