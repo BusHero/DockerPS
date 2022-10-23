81148fb3b2f9915fc70f6769cb557cb4e3c63980
bus1hero <petru.cervac@gmail.com>
2022-10-23 20:21:31 +0300

    chore: improve Install-Dependencies.ps1
    Install the modules only when they are not installed

--- src/Install-Dependencies.ps1
+++ src/Install-Dependencies.ps1
@@ -3,18 +3,35 @@ Set-PSRepository `
 	-Name PSGallery `
 	-InstallationPolicy Trusted 
 
-Write-Host 'Install Pester'
-Install-Module `
-	-Name Pester `
-	-RequiredVersion '5.3.3' `
-	-Force
-Write-Host 'Install PesterExtensions'
-Install-Module `
-	-Name PesterExtensions `
-	-RequiredVersion '0.7.4' `
-	-Force
-Write-Host 'Install PSScriptAnalyzer'
-Install-Module `
-	-Name PSScriptAnalyzer `
-	-RequiredVersion '1.21.0' `
-	-Force
+if (!(Get-InstalledModule `
+			-Name Pester `
+			-RequiredVersion '5.3.3' `
+			-ErrorAction Ignore)) {
+	Write-Host 'Install Pester'
+	Install-Module `
+		-Name Pester `
+		-RequiredVersion '5.3.3' `
+		-Force
+}
+if (!(Get-InstalledModule `
+			-Name PesterExtensions `
+			-RequiredVersion '0.7.4' `
+			-ErrorAction Ignore)) {
+	Write-Host 'Install PesterExtensions'
+	Install-Module `
+		-Name PesterExtensions `
+		-RequiredVersion '0.7.4' `
+		-Force
+} 
+
+if (!(Get-InstalledModule `
+			-Name PSScriptAnalyzer `
+			-RequiredVersion '1.21.0' `
+			-ErrorAction Ignore)) {
+	Write-Host 'Install PSScriptAnalyzer'
+	Install-Module `
+		-Name PSScriptAnalyzer `
+		-RequiredVersion '1.21.0' `
+		-Force
+}
+
