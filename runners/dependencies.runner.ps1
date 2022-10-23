92cdde3cf9d27946bc472731158aa5cbde199147
bus1hero <petru.cervac@gmail.com>
2022-10-23 21:08:49 +0300

    feat: generate test results

--- runners/dependencies.runner.ps1
+++ runners/dependencies.runner.ps1
@@ -1,10 +1,14 @@
 Import-Module Pester
-$path = "${PSScriptRoot}\..\tests\dependencies.Tests.ps1"
 
+$ProjectRoot = "${PSScriptRoot}\.."
+$path = "${ProjectRoot}\tests\dependencies.Tests.ps1"
 
 [PesterConfiguration] $configuration = New-PesterConfiguration
 
 $configuration.Run.Path = $path
 $configuration.Run.Exit = $true
 $configuration.Output.CIFormat = 'GithubActions'
+$configuration.TestResult.Enabled = $true
+$configuration.TestResult.OutputPath = "${ProjectRoot}\test-results\dependencies.Tests.$(Get-Date -Format 'yyyy_MM_dd_HH_mm_ss').xml"
+
 Invoke-Pester -Configuration $configuration
