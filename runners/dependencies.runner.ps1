8a96ad6765672350656eff69ea6aa822edfb5ad8
bus1hero <petru.cervac@gmail.com>
2022-10-23 20:48:59 +0300

    chore: add more configs to dependencies.runners.ps1

--- runners/dependencies.runner.ps1
+++ runners/dependencies.runner.ps1
@@ -1,3 +1,10 @@
+Import-Module Pester
 $path = "${PSScriptRoot}\..\tests\dependencies.Tests.ps1"
 
-Invoke-Pester -Path $path
+
+[PesterConfiguration] $configuration = New-PesterConfiguration
+
+$configuration.Run.Path = $path
+$configuration.Run.Exit = $true
+$configuration.Output.CIFormat = 'GithubActions'
+Invoke-Pester -Configuration $configuration
