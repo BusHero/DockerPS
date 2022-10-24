eecf2df7e95fd987cff4a76472d74e6252fe2756
Cervac Petru <petru.cervac@gmail.com>
2022-10-24 23:22:25 +0300

    feat: add support for nuget pack (#19)
    * chore: ignore nuspec files
    
    * feat: add a target to pack the module

--- build/Build.cs
+++ build/Build.cs
@@ -2,6 +2,8 @@ using Nuke.Common;
 using Nuke.Common.IO;
 using Nuke.Common.Tools.PowerShell;
 using static PowerShellCoreTasks;
+using static Nuke.Common.Tools.NuGet.NuGetTasks;
+using Nuke.Common.Tools.NuGet;
 
 class Build : NukeBuild
 {
@@ -38,4 +40,19 @@ class Build : NukeBuild
 		.TriggeredBy(GenerateModuleManifest)
 		.Executes(() => PowerShellCore(_ => _
 			.SetFile(RunnersPath / "test-modulemanifest.runner.ps1")));
+
+	private Target GenerateNuspec => _ => _
+		.DependsOn(GenerateModuleManifest)
+		.Executes(() => PowerShellCore(_ => _
+			.SetFile(RunnersPath / "nuspec.ps1")
+			.AddFileArguments(
+				"-ManifestPath", SrcPath / "DockerPS" / "DockerPS.psd1",
+				"-DestinationFolder", SrcPath / "DockerPS")));
+
+	private Target Pack => _ => _
+		.DependsOn(GenerateNuspec)
+		.Executes(() => NuGetPack(_ => _
+			.SetTargetPath(SrcPath / "DockerPS" / "DockerPS.nuspec")
+			.SetOutputDirectory(RootDirectory / "packages")
+			.AddProperty("NoWarn", "NU5110,NU5111,NU5125")));
 }
