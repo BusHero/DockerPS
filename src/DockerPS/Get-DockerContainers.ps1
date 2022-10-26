7c578b9721aac08b2ca457466f4560a3c7468214
bus1hero <petru.cervac@gmail.com>
2022-10-26 23:08:02 +0300

    feat: add support for filters

--- src/DockerPS/Get-DockerContainers.ps1
+++ src/DockerPS/Get-DockerContainers.ps1
@@ -1,3 +1,50 @@
+function ConvertStringFilterToArguments {
+	param (
+		$Filter
+	)
+	if ($Filter) {
+		'--filter'
+		"'${Filter}'"
+	}
+}
+
+function ConvertArrayToArguments {
+	param (
+		$Filter
+	)
+	foreach ($f in $Filter | Where-Object { $_ }) {
+		'--filter'
+		"'${f}'"
+	}
+}
+
+function ConvertHashtableToArguments {
+	param (
+		$Filter
+	)
+	foreach ($key in $Filter.Keys) {
+		if ($Filter.$key) {
+			'--filter'
+			"'${Key}=$($Filter.$Key)'"
+		}
+	}
+}
+
+function ConvertFilterToDockerArguments {
+	param (
+		$Filter
+	)
+	if ($Filter -is [string]) {
+		return ConvertStringFilterToArguments $Filter
+	}
+	elseif ($Filter -is [array]) {
+		return ConvertArrayToArguments $Filter
+	}
+	elseif ($Filter -is [hashtable] -or $Filter -is [System.Collections.Specialized.OrderedDictionary]) {
+		return ConvertHashtableToArguments $Filter
+	}
+}
+
 function Format-DockerArguments {
 	[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseSingularNouns', '', Justification = 'Demo')]
 	param (
@@ -17,6 +64,7 @@ function Format-DockerArguments {
 	if ($NoTrunc) {
 		$arguments += '--no-trunc'
 	}
+	$arguments += (ConvertFilterToDockerArguments $Filter)
 	$arguments = $arguments | Where-Object { $_ }
 	return [string]::Join(' ', $arguments)
 }
@@ -28,10 +76,13 @@ function Get-DockerContainers {
 		$containerName,
 
 		[switch]
-		$NoTrunc
+		$NoTrunc,
+
+		$Filter
 	)
 	$arguments = (Format-DockerArguments `
 			-containerName $containerName `
-			-NoTrunc:$NoTrunc)
+			-NoTrunc:$NoTrunc `
+			-Filter $Filter)
 	return docker $arguments | ConvertTo-Json
 }
--- tests/DockerPS/Get-DockerContainers.tests.ps1
+++ tests/DockerPS/Get-DockerContainers.tests.ps1
@@ -28,4 +28,79 @@ Describe 'Get-DockerContainers' {
 			"$args" -eq "container ls --format '{{json .}}' --no-trunc"
 		}
 	}
+
+	Context 'Filters' {
+		It 'one filter' {
+			$filter = 'foo=bar'
+			Get-DockerContainers -Filter $filter 
+			Should -Invoke -CommandName 'docker' -Times 1 -ParameterFilter {
+				"$args" -eq "container ls --format '{{json .}}' --filter '${filter}'"
+			}
+		}
+
+		It 'two filter' {
+			$filter1 = 'foo=bar'
+			$filter2 = 'bar=baz'
+			Get-DockerContainers -Filter $filter1, $filter2
+			Should -Invoke -CommandName 'docker' -Times 1 -ParameterFilter {
+				"$args" -eq "container ls --format '{{json .}}' --filter '${filter1}' --filter '${filter2}'"
+			}
+		}
+
+		Context 'Ignore empty filters' {
+			It 'One filter' {
+				$filter = ''
+				Get-DockerContainers -Filter $filter
+				Should -Invoke -CommandName 'docker' -Times 1 -ParameterFilter {
+					"$args" -eq "container ls --format '{{json .}}'"
+				}
+			}
+			
+			It 'Two filters' {
+				$filter1 = 'foo=bar'
+				$filter2 = ''
+				Get-DockerContainers -Filter $filter1, $filter2
+				Should -Invoke -CommandName 'docker' -Times 1 -ParameterFilter {
+					"$args" -eq "container ls --format '{{json .}}' --filter '${filter1}'"
+				}
+			}
+		}
+		Context 'Filter as dictionary' {
+			It 'One filter' {
+				$filter = @{
+					foo = 'Bar'
+				}
+				Get-DockerContainers -Filter $filter
+				Should -Invoke -CommandName 'docker' -Times 1 -ParameterFilter {
+					"$args" -eq "container ls --format '{{json .}}' --filter 'foo=$($filter.foo)'"
+				}
+			}
+
+			It 'Hashtable' {
+				$filter = @{
+					foo = 'bar'
+					bar = 'baz'
+				}
+				Get-DockerContainers -Filter $filter
+				Should -Invoke -CommandName 'docker' -Times 1 -ParameterFilter {
+					switch ("${args}") {
+						"container ls --format '{{json .}}' --filter 'foo=$($filter.foo)' --filter 'bar=$($filter.bar)'" { return $true }
+						"container ls --format '{{json .}}' --filter 'bar=$($filter.bar)' --filter 'foo=$($filter.foo)'" { return $true }
+						Default { return $false }
+					}
+				}
+			}
+
+			It 'Two filter ordered' {
+				$filter = [ordered]@{
+					foo = 'Bar'
+					bar = 'Baz'
+				}
+				Get-DockerContainers -Filter $filter
+				Should -Invoke -CommandName 'docker' -Times 1 -ParameterFilter {
+					"$args' -eq 'container ls --format '{{json .}}' --filter 'foo=$($filter.foo)' --filter 'bar=$($filter.bar)'"
+				}
+			}
+		}
+	}
 }
