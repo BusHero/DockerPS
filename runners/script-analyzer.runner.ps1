a582c913eb1edddd4631cb11374831f32e19a3cc
Cervac Petru <petru.cervac@gmail.com>
2022-10-27 00:10:40 +0300

    feat: fix analyzer runner (#38)
    Add the Get-DockerContainers script to the analyzer test

--- runners/script-analyzer.runner.ps1
+++ runners/script-analyzer.runner.ps1
@@ -3,19 +3,27 @@ Import-Module Pester
 $ProjectRoot = "${PSScriptRoot}\.."
 $date = Get-Date -Format 'yyyy_MM_dd_HH_mm_ss'
 
-[PesterConfiguration] $configuration = New-PesterConfiguration
 
-[Pester.ContainerInfo]$container = New-PesterContainer `
-	-Path "${ProjectRoot}\tests\analyzer.tests.ps1"
+$files = @(
+	"${ProjectRoot}\src\DockerPS\Get-DockerImages.ps1",
+	"${ProjectRoot}\src\DockerPS\Get-DockerContainers.ps1"
+)
 
-$container.Data = @{
-	TestLocation = "${ProjectRoot}\src\DockerPS\Get-DockerImages.ps1"
+foreach ($file in $files) {
+	[PesterConfiguration] $configuration = New-PesterConfiguration
+	
+	[Pester.ContainerInfo]$container = New-PesterContainer `
+		-Path "${ProjectRoot}\tests\analyzer.tests.ps1"
+	
+	$container.Data = @{
+		TestLocation = $file
+	}
+	
+	$configuration.Run.Container = $container
+	$configuration.Run.Exit = $true
+	$configuration.TestResult.Enabled = $true
+	$configuration.TestResult.OutputPath = `
+		"${ProjectRoot}\test-results\analyzer.${file}.${date}.xml"
+	
+	Invoke-Pester -Configuration $configuration
 }
-
-$configuration.Run.Container = $container
-$configuration.Run.Exit = $true
-$configuration.TestResult.Enabled = $true
-$configuration.TestResult.OutputPath = `
-	"${ProjectRoot}\test-results\analyzer.${date}.xml"
-
-Invoke-Pester -Configuration $configuration
