4a9589d40d74c3e9c8fce60c9f4d30af58ed44fd
bus1hero <petru.cervac@gmail.com>
2022-10-30 15:56:40 +0200

    feat: fix the private functions

--- src/DockerPS/Get-DockerContainers.ps1
+++ src/DockerPS/Get-DockerContainers.ps1
@@ -1,74 +1,3 @@
-function ConvertStringFilterToArguments {
-	param (
-		$Filter
-	)
-	if ($Filter) {
-		'--filter'
-		"'${Filter}'"
-	}
-}
-
-function ConvertArrayToArguments {
-	param (
-		$Filter
-	)
-	foreach ($f in $Filter | Where-Object { $_ }) {
-		'--filter'
-		"'${f}'"
-	}
-}
-
-function ConvertHashtableToArguments {
-	param (
-		$Filter
-	)
-	foreach ($key in $Filter.Keys) {
-		if ($Filter.$key) {
-			'--filter'
-			"'${Key}=$($Filter.$Key)'"
-		}
-	}
-}
-
-function ConvertFilterToDockerArguments {
-	param (
-		$Filter
-	)
-	if ($Filter -is [string]) {
-		return ConvertStringFilterToArguments $Filter
-	}
-	elseif ($Filter -is [array]) {
-		return ConvertArrayToArguments $Filter
-	}
-	elseif ($Filter -is [hashtable] -or $Filter -is [System.Collections.Specialized.OrderedDictionary]) {
-		return ConvertHashtableToArguments $Filter
-	}
-}
-
-function Format-DockerArguments {
-	[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseSingularNouns', '', Justification = 'Demo')]
-	param (
-		[string]
-		$containerName,
-
-		[switch]
-		$NoTrunc
-	)
-	$arguments = @()
-
-	$arguments += 'container'
-	$arguments += 'ls'
-	$arguments += '--format'
-	$arguments += "'{{json .}}'"
-	$arguments += $containerName
-	if ($NoTrunc) {
-		$arguments += '--no-trunc'
-	}
-	$arguments += (ConvertFilterToDockerArguments $Filter)
-	$arguments = $arguments | Where-Object { $_ }
-	return [string]::Join(' ', $arguments)
-}
-
 function Get-DockerContainers {
 	[CmdletBinding(HelpUri = 'https://github.com/BusHero/DockerPS/wiki/Get-DockerContainers')]
 	[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseSingularNouns', '', Justification = 'Demo')]
@@ -81,6 +10,78 @@ function Get-DockerContainers {
 
 		$Filter
 	)
+
+	function ConvertStringFilterToArguments {
+		param (
+			$Filter
+		)
+		if ($Filter) {
+			'--filter'
+			"'${Filter}'"
+		}
+	}
+	
+	function ConvertArrayToArguments {
+		param (
+			$Filter
+		)
+		foreach ($f in $Filter | Where-Object { $_ }) {
+			'--filter'
+			"'${f}'"
+		}
+	}
+	
+	function ConvertHashtableToArguments {
+		param (
+			$Filter
+		)
+		foreach ($key in $Filter.Keys) {
+			if ($Filter.$key) {
+				'--filter'
+				"'${Key}=$($Filter.$Key)'"
+			}
+		}
+	}
+	
+	function ConvertFilterToDockerArguments {
+		param (
+			$Filter
+		)
+		if ($Filter -is [string]) {
+			return ConvertStringFilterToArguments $Filter
+		}
+		elseif ($Filter -is [array]) {
+			return ConvertArrayToArguments $Filter
+		}
+		elseif ($Filter -is [hashtable] -or $Filter -is [System.Collections.Specialized.OrderedDictionary]) {
+			return ConvertHashtableToArguments $Filter
+		}
+	}
+	
+	function Format-DockerArguments {
+		[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseSingularNouns', '', Justification = 'Demo')]
+		param (
+			[string]
+			$containerName,
+	
+			[switch]
+			$NoTrunc
+		)
+		$arguments = @()
+	
+		$arguments += 'container'
+		$arguments += 'ls'
+		$arguments += '--format'
+		$arguments += "'{{json .}}'"
+		$arguments += $containerName
+		if ($NoTrunc) {
+			$arguments += '--no-trunc'
+		}
+		$arguments += (ConvertFilterToDockerArguments $Filter)
+		$arguments = $arguments | Where-Object { $_ }
+		return [string]::Join(' ', $arguments)
+	}
+
 	$arguments = (Format-DockerArguments `
 			-containerName $containerName `
 			-NoTrunc:$NoTrunc `
--- src/DockerPS/Get-DockerImages.ps1
+++ src/DockerPS/Get-DockerImages.ps1
@@ -1,77 +1,3 @@
-function ConvertStringFilterToArguments {
-	param (
-		$Filter
-	)
-	if ($Filter) {
-		'--filter'
-		"'${Filter}'"
-	}
-}
-
-function ConvertArrayToArguments {
-	param (
-		$Filter
-	)
-	foreach ($f in $Filter | Where-Object { $_ }) {
-		'--filter'
-		"'${f}'"
-	}
-}
-
-function ConvertHashtableToArguments {
-	param (
-		$Filter
-	)
-	foreach ($key in $Filter.Keys) {
-		if ($Filter.$key) {
-			'--filter'
-			"'${Key}=$($Filter.$Key)'"
-		}
-	}
-}
-
-function ConvertFilterToDockerArguments {
-	param (
-		$Filter
-	)
-	if ($Filter -is [string]) {
-		return ConvertStringFilterToArguments $Filter
-	}
-	elseif ($Filter -is [array]) {
-		return ConvertArrayToArguments $Filter
-	}
-	elseif ($Filter -is [hashtable] -or $Filter -is [System.Collections.Specialized.OrderedDictionary]) {
-		return ConvertHashtableToArguments $Filter
-	}
-}
-
-function Format-DockerArguments {
-	[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseSingularNouns', '', Justification = 'Demo')]
-	param (
-		[string]
-		$Image,
-
-		[switch]
-		$NoTrunc,
-
-		$Filter
-	)
-	$arguments = @()
-
-	$arguments += 'images'
-	$arguments += '--format'
-	$arguments += "'{{json .}}'"
-	$arguments += $Image
-	if ($NoTrunc) {
-		$arguments += '--no-trunc'
-	}
-
-	$arguments += (ConvertFilterToDockerArguments $Filter)
-
-	$arguments = $arguments | Where-Object { $_ }
-	return [string]::Join(' ', $arguments)
-}
-
 function Get-DockerImages {
 	[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseSingularNouns', '', Justification = 'Demo')]
 	[CmdletBinding(HelpUri = 'https://github.com/BusHero/DockerPS/wiki/Get-DockerImages')]
@@ -84,6 +10,81 @@ function Get-DockerImages {
 
 		$Filter
 	)
+
+	function ConvertStringFilterToArguments {
+		param (
+			$Filter
+		)
+		if ($Filter) {
+			'--filter'
+			"'${Filter}'"
+		}
+	}
+	
+	function ConvertArrayToArguments {
+		param (
+			$Filter
+		)
+		foreach ($f in $Filter | Where-Object { $_ }) {
+			'--filter'
+			"'${f}'"
+		}
+	}
+	
+	function ConvertHashtableToArguments {
+		param (
+			$Filter
+		)
+		foreach ($key in $Filter.Keys) {
+			if ($Filter.$key) {
+				'--filter'
+				"'${Key}=$($Filter.$Key)'"
+			}
+		}
+	}
+	
+	function ConvertFilterToDockerArguments {
+		param (
+			$Filter
+		)
+		if ($Filter -is [string]) {
+			return ConvertStringFilterToArguments $Filter
+		}
+		elseif ($Filter -is [array]) {
+			return ConvertArrayToArguments $Filter
+		}
+		elseif ($Filter -is [hashtable] -or $Filter -is [System.Collections.Specialized.OrderedDictionary]) {
+			return ConvertHashtableToArguments $Filter
+		}
+	}
+	
+	function Format-DockerArguments {
+		[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseSingularNouns', '', Justification = 'Demo')]
+		param (
+			[string]
+			$Image,
+	
+			[switch]
+			$NoTrunc,
+	
+			$Filter
+		)
+		$arguments = @()
+	
+		$arguments += 'images'
+		$arguments += '--format'
+		$arguments += "'{{json .}}'"
+		$arguments += $Image
+		if ($NoTrunc) {
+			$arguments += '--no-trunc'
+		}
+	
+		$arguments += (ConvertFilterToDockerArguments $Filter)
+	
+		$arguments = $arguments | Where-Object { $_ }
+		return [string]::Join(' ', $arguments)
+	}
+	
 	$arguments = (Format-DockerArguments -Image $Image -NoTrunc:$NoTrunc -Filter $Filter)
 	return docker $arguments | ConvertFrom-Json
 }
