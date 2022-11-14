0bc73b8a2c9fad2fdbdef84151e5713a93059db8
Cervac Petru <petru.cervac@gmail.com>
2022-11-14 21:51:42 +0200

    ci: make the setup.ps1 script to work without prerelease (#57)
    * ci: make the setup.ps1 script to work without prerelease
    
    * ci: fix setup.ps1

--- runners/setup.ps1
+++ runners/setup.ps1
@@ -6,21 +6,25 @@ param(
 	[Parameter(Mandatory = $false)]
 	[string]
 	$Prerelease
-
 )
 
 $constants = & "${PSScriptRoot}\..\constants.ps1"
 $manifestPath = "${PSScriptRoot}\..\src\$($constants.ProjectName)\$($constants.ProjectName).psd1"
 
-New-ModuleManifest `
-	-Guid $constants.ProjectGUID `
-	-Path $manifestPath `
-	-Description 'Some description here and there' `
-	-Author $constants.Author `
-	-ProjectUri $constants.Repository `
-	-LicenseUri $constants.LicenseUri `
-	-RootModule "$($constants.ProjectName).psm1" `
-	-ModuleVersion $version `
-	-Prerelease $Prerelease `
-	-Tags 'docker' `
-	-FunctionsToExport 'Get-DockerImages', 'Get-DockerContainers'
+$moduleManifestArgs = @{
+	Guid              = $constants.ProjectGUID
+	Path              = $manifestPath
+	Description       = 'Some description here and there'
+	Author            = $constants.Author
+	ProjectUri        = $constants.Repository
+	LicenseUri        = $constants.LicenseUri
+	RootModule        = "$($constants.ProjectName).psm1"
+	ModuleVersion     = $version
+	Tags              = 'docker'
+	FunctionsToExport = 'Get-DockerImages', 'Get-DockerContainers'
+}
+if ($Prerelease) {
+	$moduleManifestArgs.Prerelease = $Prerelease
+}
+
+New-ModuleManifest @moduleManifestArgs
